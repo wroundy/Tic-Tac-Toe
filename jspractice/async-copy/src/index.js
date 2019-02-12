55cc0423db3db3e402c22027edd2876c4b8e5853
Wroundy <roundywilliam97@gmail.com>
2019-02-12 13:18:06 -0700

    second commit

---
+++ jspractice/async-copy/src/index.js
@@ -0,0 +1,24 @@
+/**
+ * Create a string with your name, wrap it with a promise, and return it.
+ */
+export const resolvingValues = () => {
+  return 
+}
+
+/**
+ * Return a rejected promise that has 'Doh' as the message
+ */
+export const rejectedPromise = () => {
+  return REPLACE_WITH_CODE
+}
+
+/**
+ * For this function, we're going to some code that conditionally checks the number (parameter)
+ *  - When the number is positive, resolve the promise with the number
+ *  - When the number is negative, reject the promise, provide the number as the rejection message
+ */
+export const conditionallyReject = (number = 0) => {
+  return REPLACE_WITH_CODE
+}
+
+const REPLACE_WITH_CODE = false
